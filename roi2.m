1a7f346c0851b40dea92e6989618b31799ff63e1
Deeprotein <145230688+Deeprotein@users.noreply.github.com>
2026-01-08 12:04:51 -0500

    Delete roi2.m

--- roi2.m
+++
@@ -1,7 +0,0 @@
-% Load the ROI from NIfTI (.nii or .nii.gz)
-roi_nifti = niftiread('C:\Aliye\HFHS\MRI\Pilot\Correct Files\patient 017\09062019\binary segmentation\patient017_label2.nii.gz'); 
-
-total = roi_nifti;
-
-output_path = 'C:\Aliye\HFHS\MRI\SERA\Aliye\nifti\Recurrence FU\62875676_17\09062019\2\contours.mat';
-save(output_path, 'total');
